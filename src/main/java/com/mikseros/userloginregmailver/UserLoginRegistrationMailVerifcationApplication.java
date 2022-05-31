ced9508f9098edeaee4e17fc17718a67bd9bf306
mikseros <mmadynski@gmail.com>
2022-05-31 22:25:03 +0200

    first commit

---
+++ src/main/java/com/mikseros/userloginregmailver/UserLoginRegistrationMailVerifcationApplication.java
@@ -0,0 +1,13 @@
+package com.mikseros.userloginregmailver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLoginRegistrationMailVerifcationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserLoginRegistrationMailVerifcationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikseros/userloginregmailver/UserLoginRegistrationMailVerifcationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikseros.userloginregmailver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLoginRegistrationMailVerifcationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
